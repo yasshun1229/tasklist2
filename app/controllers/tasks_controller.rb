998533d664b6ee611171b13a768d312f07b26043
shunsuke.yasuda <69839892+yasshun1229@users.noreply.github.com>
2021-10-24 09:25:22 +0000

    f

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all # taskを全て取得する
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -24,12 +25,9 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
-    
     if @task.update(task_params)
       flash[:success] = "タスクは更新されました"
       redirect_to @task
@@ -40,16 +38,19 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = "タスクは削除されました"
     redirect_to root_url #リダイレクトの場合は、〇〇_path禁止
   end
-end
 
-private
-
-def task_params
-  params.require(:task).permit(:content, :status)
+  private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
 end
